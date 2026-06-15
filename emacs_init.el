a3333da2d88497d43df20f8a2f49a9397f74653f
st22182ti <st22182ti@gm.ibaraki-ct.ac.jp>
2026-06-15 11:20:35 +0900

    emacs tab-bar & proxy

--- emacs_init.el
+++ emacs_init.el
@@ -4,18 +4,19 @@
 (setq-default truncate-lines t)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("8dbbcb2b7ea7e7466ef575b60a92078359ac260c91fe908685b3983ab8e20e3f"
      default))
  '(display-line-numbers-width-start t)
  '(package-selected-packages
-   '(catppuccin-theme evil geiser-gauche monokai-theme rust-mode sly)))
+   '(catppuccin-theme centaur-tabs evil geiser-gauche monokai-theme
+		      rust-mode sly)))
 
-;; Smooth Scrolloing
+
+;; ============================================================
+;; Smooth Scrolling
+;; ============================================================
+
 (if (fboundp 'pixel-scroll-precision-mode)
     (pixel-scroll-precision-mode 1))
 (setq scroll-conservatively 101)       
@@ -24,16 +25,34 @@
 (setq fast-but-imprecise-scrolling t)
 (setq redisplay-skip-initial-frame t)
 
+
+
+;; ============================================================
+;; Package Manager
+;; ============================================================
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
+
+
+
+;; ============================================================
+;; Color Scheme
+;; ============================================================
 
 (use-package catppuccin-theme
   :ensure t
   :init
   (setq catppuccin-flavor 'mocha)
   (load-theme 'catppuccin :no-confirm))
+
+
+
+;; ============================================================
+;; VIM keybind
+;; ============================================================
 
 (use-package evil
   :ensure t
@@ -42,6 +61,55 @@
   (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
+
+
+
+;; ============================================================
+;; Tab bar
+;; ============================================================
+
+(use-package centaur-tabs
+  :ensure t
+  :config
+  (centaur-tabs-mode 1))
+
+(with-eval-after-load 'evil
+  (with-eval-after-load 'centaur-tabs
+    (evil-define-key 'normal 'global (kbd "H") 'centaur-tabs-backward)
+    (evil-define-key 'normal 'global (kbd "L") 'centaur-tabs-forward)))
+
+
+
+;; ============================================================
+;; HTTP Proxy
+;; ============================================================
+
+(defvar proxy-url "po.cc.ibaraki-ct.ac.jp:3128")
+
+(defun proxy-on ()
+  (interactive)
+  (setq url-proxy-services
+	'(("http" . proxy-url)
+	  ("https" . proxy-url)
+	  ("no_proxy" . "^\\(localhost\\|127\\.0\\.0\\.1\\)")))
+  (setenv "http_proxy" (concat "http://" proxy-url))
+  (setenv "https_proxy" (concat "http://" proxy-url))
+  (setenv "no_proxy" "localhost,127.0.0.1")
+  (message "Proxy enabled: %s" (concat "http://" proxy-url)))
+
+(defun proxy-off ()
+  (interactive)
+  (setq url-proxy-services nil)
+  (setenv "http_proxy" nil)
+  (setenv "https_proxy" nil)
+  (setenv "no_proxy" nil)
+  (message "Proxy disabled"))
+
+
+
+;; ============================================================
+;; LSP
+;; ============================================================
 
 (use-package eglot
   :ensure t
